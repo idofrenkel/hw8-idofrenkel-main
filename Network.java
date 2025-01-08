cc4f1af25b748134ed2c63fdfba8db7d218fe975
Ido Frenkel <ido.frenkel@post.runi.ac.il>
2025-01-08 16:51:38 +0200

    print fix

--- Network.java
+++ Network.java
@@ -134,7 +134,7 @@ public class Network {
     // Returns a textual description of all the users in this network, and who they follow.
     
     public String toString() {
-    String ans = "Network";
+    String ans = "Network:";
 
         for (int i = 0; i < userCount; i++){
             ans = ans + "\n" + users[i].toString();
